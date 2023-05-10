7196913e7aaa3de5e446d2f3ba4ac043a73aee43
dpreid <dprydereid@gmail.com>
2023-05-10 10:59:31 +0100

    migrated to vite

--- dashboard/vue.config.js
+++
@@ -1,8 +0,0 @@
-// vue.config.js
-module.exports = {
-    // options...
-    runtimeCompiler: true,
-    publicPath: process.env.NODE_ENV === 'production'
-    ? '/ui/analytics-1.0/'
-    : '/'
-  }
